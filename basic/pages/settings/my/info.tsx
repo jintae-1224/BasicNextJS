54773bf0c71e21764309f40eeb963357c0097b4a
jintae-1224 <kab7210@naver.com>
2022-12-01 00:36:37 +0900

    Shallow Routing

--- basic/pages/settings/my/info.tsx
+++ basic/pages/settings/my/info.tsx
@@ -1,11 +1,55 @@
+import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import Layout from '../../../components/Layout'
 import SubLayout from '../../../components/SubLayout'
 
+export async function getServerSideProps() {
+  console.log('server')
+  return {
+    props: {},
+  }
+}
+
 export default function Info() {
+  const router = useRouter()
+  const [click, setClick] = useState<boolean>(false)
+  const { status = 'initial' } = router.query
   return (
     <div>
       <h1>Info</h1>
+      <h1>Clicked {click.toString()}</h1>
+      <h1>status {status}</h1>
+      <button
+        onClick={() => {
+          alert('edit'),
+            setClick(true),
+            location.replace('/settings/my/info?status=editing')
+        }}
+      >
+        edit(repalce)
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          alert('edit'),
+            setClick(true),
+            router.push('/settings/my/info?status=editing')
+        }}
+      >
+        edit(repalce)
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          alert('edit'),
+            setClick(true),
+            router.push('/settings/my/info?status=editing', undefined, {
+              shallow: true,
+            })
+        }}
+      >
+        edit(repalce)
+      </button>
     </div>
   )
 }

5d738c66bb51091c5e175b3421aa5fc166157570
jintae-1224 <kab7210@naver.com>
2022-12-03 22:37:35 +0900

    api

--- basic/pages/[username]/[info].tsx
+++ basic/pages/[username]/[info].tsx
@@ -6,10 +6,21 @@ import SubLayout from '../../components/SubLayout'
 export default function UserNameInfo() {
   const router = useRouter()
   const { username, info } = router.query
+  const [name, setName] = useState<string>('')
+
+  useEffect(() => {
+    fetch('/api/user')
+      .then((res) => res.json())
+      .then((data) => {
+        setName(data.name)
+      })
+  }, [])
+
   return (
     <div>
       <h1>UserName : {username}</h1>
       <h1>info : {info}</h1>
+      <h1>Name : {name}</h1>
     </div>
   )
 }
